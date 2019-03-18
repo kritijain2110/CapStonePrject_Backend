2dbdd2af7befae0571fe27e4ea816f4da12de1b4
Saatvik Jain <saatvik16261@iiitd.ac.in>
2019-03-18 10:34:16 +0530

    Added controller class RestaurantController. Added functionality in controller class to return JSON query with all restaurant details for "\restaurant"

---
+++ FoodOrderingAppBackend-STUB/FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/RestaurantController.java
@@ -0,0 +1,39 @@
+@RequestMapping("/")
+public class RestaurantController implements EndPointIdentifier {
+
+    // Implemented Endpoint Identifier interface for generic AuthorizationFailedException Handling
+
+    @Autowired
+    RestaurantService restaurantService;
+
+    /**
+     * Method implements the get all resturantd endpoint
+     *
+     * @param accessToken assigned to the user upon signin
+     * @return ResponseEntity to indicate the status of the query as well as the list of questions
+     * @throws AuthorizationFailedException
+     */
+
+    @GetMapping(path = "/restaurant", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<List<RestaurantDetailsResponse>> getAllRestaurants(@RequestHeader("authorization") String accessToken) throws AuthorizationFailedException {
+
+        List<RestaurantEntity> restaurantEntityList = restaurantService.getAllRestaurants(accessToken);
+
+        List<RestaurantDetailsResponse> restaurantDetailsResponseList = new ArrayList<RestaurantDetailsResponse>();
+        if (!restaurantEntityList.isEmpty()) {
+
+            for (RestaurantEntity n : restaurantEntityList) {
+                RestaurantDetailsResponse restaurantDetailsResponse = new RestaurantDetailsResponse();
+               // restaurantDetailsResponse.setId(n.getUuid());
+               // restaurantDetailsResponse.setContent(n.getContent());
+
+                restaurantDetailsResponseList.add(restaurantDetailsResponse);
+            }
+
+        }
+
+        return new ResponseEntity<>(restaurantDetailsResponseList, HttpStatus.OK);
+
+    }
+
+
