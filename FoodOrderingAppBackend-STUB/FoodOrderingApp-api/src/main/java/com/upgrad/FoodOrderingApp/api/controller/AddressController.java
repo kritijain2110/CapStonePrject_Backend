d5f0e7858a92c5d943da3615fa0e27c6e3976609
anupriyakhare34 <anupriya.khare34@gmail.com>
2019-03-24 18:52:30 +0530

    commit 11
    created address entity and state entity

--- FoodOrderingAppBackend-STUB/FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/AddressController.java
+++ FoodOrderingAppBackend-STUB/FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/AddressController.java
@@ -1,4 +1,9 @@
 package com.upgrad.FoodOrderingApp.api.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
 public class AddressController {
 }
